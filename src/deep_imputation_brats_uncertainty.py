49697155edd82334a52da6f109211e5bae6e01ae
Sachin Salim <sachinksalim@gmail.com>
2024-10-20 18:59:28 +0000

    added python script version of the notebook

---
+++ src/deep_imputation_brats_uncertainty.py
@@ -0,0 +1,519 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Deep Imputation of BraTS dataset with MONAI
+# 
+# The dataset comes from http://medicaldecathlon.com/.  
+# Modality: Multimodal multisite MRI data (FLAIR, T1w, T1gd,T2w)  
+# Size: 750 4D volumes (484 Training + 266 Testing)  
+# Source: BRATS 2016 and 2017 datasets.  
+# Challenge: **Drop some of the modalities randomly and reconstruct it by imputing with a 3D U-Net**
+
+# ## Check if this is a notebook or not
+
+# In[2]:
+
+
+def is_notebook():
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True  # Jupyter notebook or Jupyter QtConsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other types
+    except NameError:
+        return False  # Standard Python interpreter
+
+# Example usage
+if is_notebook():
+    print("This is a Jupyter Notebook.")
+else:
+    print("This is not a Jupyter Notebook.")
+
+
+# ## Setup environment
+
+# In[ ]:
+
+
+get_ipython().system('python -c "import monai" || pip install -q "monai-weekly[nibabel, tqdm]"')
+get_ipython().system('python -c "import matplotlib" || pip install -q matplotlib')
+# !python -c "import onnxruntime" || pip install -q onnxruntime
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# ## Setup imports
+
+# In[ ]:
+
+
+import os
+import shutil
+import tempfile
+import time
+import matplotlib.pyplot as plt
+from monai.config import print_config
+from monai.transforms import (
+    Activations,
+    AsDiscrete,
+    Compose,
+)
+from monai.networks.nets import UNet
+from monai.transforms import (
+    LoadImage,
+    NormalizeIntensity,
+    Orientation,
+    RandFlip,
+    RandScaleIntensity,
+    RandShiftIntensity,
+    RandSpatialCrop,
+    Spacing,
+    EnsureType,
+    EnsureChannelFirst,
+)
+from monai.metrics import MSEMetric
+from monai.utils import set_determinism
+from tqdm import tqdm
+
+import pdb
+import os
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader, Subset
+import numpy as np
+import json
+
+print_config()
+
+
+# ## Setup data directory
+# 
+# You can specify a directory with the `MONAI_DATA_DIRECTORY` environment variable.  
+# This allows you to save results and reuse downloads.  
+# If not specified a temporary directory will be used.
+
+# In[ ]:
+
+
+os.environ["MONAI_DATA_DIRECTORY"] = "/scratch1/sachinsa/monai_data_1"
+
+
+# In[ ]:
+
+
+directory = os.environ.get("MONAI_DATA_DIRECTORY")
+if directory is not None:
+    os.makedirs(directory, exist_ok=True)
+root_dir = tempfile.mkdtemp() if directory is None else directory
+print(root_dir)
+
+
+# In[ ]:
+
+
+run_id = 1 # set this to prevent overlapped saving of model and data
+save_dir = os.path.join(root_dir, f"run_{run_id}")
+os.makedirs(save_dir, exist_ok=True)
+
+IS_NOTEBOOK = True # set this if this is a jupyter notebook. False if a python script
+
+
+# ## Set parameters
+
+# In[ ]:
+
+
+max_epochs = 150 # 300
+val_interval = 2
+
+
+# ## Set deterministic training for reproducibility
+
+# In[ ]:
+
+
+set_determinism(seed=0)
+
+
+# Challenge (unsolved): How to ensure only input images have some modalities dropped, and output modalities have the entire data?
+
+# ## Setup transforms for training and validation
+# 
+
+# In[ ]:
+
+
+train_transform = Compose(
+    [
+        # load 4 Nifti images and stack them together
+        LoadImage(),
+        EnsureChannelFirst(),
+        EnsureType(),
+        Orientation(axcodes="RAS"),
+        Spacing(
+            pixdim=(1.0, 1.0, 1.0),
+            mode=("bilinear", "nearest"),
+        ),
+        RandSpatialCrop(roi_size=[224, 224, 144], random_size=False),
+        RandFlip(prob=0.5, spatial_axis=0),
+        RandFlip(prob=0.5, spatial_axis=1),
+        RandFlip(prob=0.5, spatial_axis=2),
+        NormalizeIntensity(nonzero=True, channel_wise=True),
+        RandScaleIntensity(factors=0.1, prob=1.0),
+        RandShiftIntensity(offsets=0.1, prob=1.0),
+    ]
+)
+val_transform = Compose(
+    [
+        LoadImage(),
+        EnsureChannelFirst(),
+        EnsureType(),
+        Orientation(axcodes="RAS"),
+        Spacing(
+            pixdim=(1.0, 1.0, 1.0),
+            mode=("bilinear", "nearest"),
+        ),
+        NormalizeIntensity(nonzero=True, channel_wise=True),
+    ]
+)
+
+
+# ## Custom Dataset to load MRI and mask
+
+# In[ ]:
+
+
+class BrainMRIDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = os.path.join(root_dir, "Task01_BrainTumour")
+        json_file_path = os.path.join(self.root_dir, "dataset.json")
+        with open(json_file_path, 'r') as file:
+            data_json = json.load(file)
+
+        self.image_filenames = data_json['training']
+
+        np.random.seed(0)
+        self.seq_mask = np.random.rand(len(self.image_filenames), 4) < 0.2
+
+        self.transform = transform
+
+    def __len__(self):
+        """Returns the total number of samples in the dataset."""
+        return len(self.image_filenames)
+
+    def __getitem__(self, idx):
+        img_name = os.path.normpath(os.path.join(self.root_dir,self.image_filenames[idx]['image']))
+        mask = self.seq_mask[idx]
+        
+        if self.transform:
+            image = self.transform(img_name)
+
+        mask = torch.from_numpy(mask)
+
+        return {"image":image, "mask":mask}
+    
+
+
+sample_ds = BrainMRIDataset(
+    root_dir=root_dir,
+    transform=train_transform
+)
+sample_loader = DataLoader(sample_ds, batch_size=2, shuffle=True, num_workers=4)
+
+
+# Consider a subset of train and validation dataset for debugging the training workflow
+
+# In[ ]:
+
+
+train_subs = Subset(sample_ds, list(range(200)))
+val_subs = Subset(sample_ds, list(range(40)))
+
+train_loader = DataLoader(train_subs, batch_size=1, shuffle=True, num_workers=2)
+val_loader = DataLoader(val_subs, batch_size=1, shuffle=False, num_workers=2)
+print(len(train_loader), len(val_loader))
+
+
+# ## Check data shape and visualize
+
+# In[ ]:
+
+
+# pick one image from DecathlonDataset to visualize and check the 4 channels
+if is_notebook():
+    val_data_example = sample_ds[6]
+    print(f"image shape: {val_data_example['image'].shape}")
+    plt.figure("image", (24, 6))
+    for i in range(4):
+        plt.subplot(1, 4, i + 1)
+        plt.title(f"image channel {i}")
+        plt.imshow(val_data_example["image"][i, :, :, 60].detach().cpu(), cmap="gray")
+        plt.colorbar()
+    plt.show()
+
+
+# ## Create Model, Loss, Optimizer
+
+# **Define a 3D Unet**
+
+# In[ ]:
+
+
+device = torch.device("cuda:0")
+model = UNet(
+    spatial_dims=3, # 3D
+    in_channels=4,
+    out_channels=8, # we will output estimated mean and estimated std dev for all 4 image channels
+    channels=(4, 8, 16),
+    strides=(2, 2),
+    num_res_units=2
+).to(device)
+
+
+# In[ ]:
+
+
+def GaussianLikelihood(expected_img, output_img):
+    # input is 4 channel images, output is 8 channel images
+    # TODO (DONE): Deal sigma<=0 issue by either initializing to 1 or adding a small constant
+    #   Dealt it using sigma
+
+    output_img_mean = output_img[:, :4, ...]
+    output_img_log_std = output_img[:, 4:, ...]
+    cost1 = (expected_img - output_img_mean)**2 / (2*torch.exp(2*output_img_log_std))
+
+    cost2 = output_img_log_std
+
+    return torch.mean(cost1 + cost2)
+
+
+# In[ ]:
+
+
+VAL_AMP = True
+
+# Define the loss function
+loss_function = GaussianLikelihood #nn.MSELoss()
+# loss_function = DiceLoss(smooth_nr=0, smooth_dr=1e-5, squared_pred=True, to_onehot_y=False, sigmoid=True)
+optimizer = torch.optim.Adam(model.parameters(), 1e-4, weight_decay=1e-5)
+lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=max_epochs)
+
+# mse_metric = DiceMetric(include_background=True, reduction="mean")
+# mse_metric_batch = DiceMetric(include_background=True, reduction="mean_batch")
+
+mse_metric = MSEMetric(reduction="mean")
+mse_metric_batch = MSEMetric(reduction="mean_batch")
+
+post_trans = Compose([Activations(sigmoid=True), AsDiscrete(threshold=0.5)])
+
+
+# define inference method
+def inference(input):
+    def _compute(input):
+        output = model(input)
+        return output
+
+    if VAL_AMP:
+        with torch.cuda.amp.autocast():
+            return _compute(input)
+    else:
+        return _compute(input)
+
+
+# use amp to accelerate training
+scaler = torch.cuda.amp.GradScaler()
+# enable cuDNN benchmark
+torch.backends.cudnn.benchmark = True
+
+
+# ### Run the following cells only if performing a new trial/run
+
+# In[ ]:
+
+
+best_metric = -1
+best_metric_epoch = -1
+best_metrics_epochs_and_time = [[], [], []]
+epoch_loss_values = []
+metric_values = []
+metric_values_tc = []
+metric_values_wt = []
+metric_values_et = []
+
+total_start = time.time()
+for epoch in range(max_epochs):
+    epoch_start = time.time()
+    print("-" * 10)
+    print(f"epoch {epoch + 1}/{max_epochs}")
+    model.train()
+    epoch_loss = 0
+    step = 0
+    for batch_data in train_loader:
+        step_start = time.time()
+        step += 1
+        inputs, mask = (
+            batch_data["image"].to(device),
+            batch_data["mask"].to(device),
+        )
+        optimizer.zero_grad()
+        with torch.cuda.amp.autocast():
+            outputs_gt = inputs.clone()
+            inputs = inputs*~mask[:,:,None,None,None]
+            outputs = model(inputs)
+            loss = loss_function(outputs_gt, outputs)
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+        epoch_loss += loss.item()
+        print(
+            f"{step}/{len(train_subs) // train_loader.batch_size}"
+            f", train_loss: {loss.item():.4f}"
+            f", step time: {(time.time() - step_start):.4f}"
+        )
+    lr_scheduler.step()
+    epoch_loss /= step
+    epoch_loss_values.append(epoch_loss)
+    print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
+
+    if (epoch + 1) % val_interval == 0:
+        model.eval()
+        with torch.no_grad():
+            for val_data in val_loader:
+                val_inputs, val_mask = (
+                    batch_data["image"].to(device),
+                    batch_data["mask"].to(device),
+                )
+                val_outputs_gt = val_inputs.clone()
+                val_inputs = val_inputs*~mask[:,:,None,None,None]
+                val_outputs = inference(val_inputs)
+                val_outputs = val_outputs[:,:4,...]
+                # val_outputs = [post_trans(i) for i in val_outputs]
+                mse_metric(y_pred=val_outputs, y=val_outputs_gt)
+                mse_metric_batch(y_pred=val_outputs, y=val_outputs_gt)
+
+            metric = mse_metric.aggregate().item()
+            metric_values.append(metric)
+            metric_batch = mse_metric_batch.aggregate()
+            mse_metric.reset()
+            mse_metric_batch.reset()
+
+            if metric > best_metric:
+                best_metric = metric
+                best_metric_epoch = epoch + 1
+                best_metrics_epochs_and_time[0].append(best_metric)
+                best_metrics_epochs_and_time[1].append(best_metric_epoch)
+                best_metrics_epochs_and_time[2].append(time.time() - total_start)
+                torch.save(
+                    model.state_dict(),
+                    os.path.join(save_dir, "best_metric_model.pth"),
+                )
+                print("saved new best metric model")
+            print(
+                f"current epoch: {epoch + 1} current mean mse: {metric:.4f}"
+                f"\nbest mean metric: {best_metric:.4f}"
+                f" at epoch: {best_metric_epoch}"
+            )
+    print(f"time consuming of epoch {epoch + 1} is: {(time.time() - epoch_start):.4f}")
+total_time = time.time() - total_start
+
+
+# In[ ]:
+
+
+print(f"train completed, best_metric: {best_metric:.4f} at epoch: {best_metric_epoch}, total time: {total_time}.")
+
+
+# In[ ]:
+
+
+# Save the loss list
+np.save(os.path.join(save_dir, 'epoch_loss_values.npy'), np.array(epoch_loss_values))
+np.save(os.path.join(save_dir, 'metric_values.npy'), np.array(metric_values))
+del epoch_loss_values, metric_values
+
+
+# ### Run from here to re-plot the loss
+
+# In[ ]:
+
+
+# Load the save list
+epoch_loss_values = np.load(os.path.join(save_dir, 'epoch_loss_values.npy')).tolist()
+metric_values = np.load(os.path.join(save_dir, 'metric_values.npy')).tolist()
+
+print(epoch_loss_values)
+print(metric_values)
+
+
+# ## Plot the loss and metric
+
+# In[ ]:
+
+
+if is_notebook():
+    plt.figure("train", (12, 6))
+    plt.subplot(1, 2, 1)
+    plt.title("Epoch Average Loss")
+    x = [i + 1 for i in range(len(epoch_loss_values))]
+    y = epoch_loss_values
+    plt.xlabel("epoch")
+    plt.plot(x, y, color="red")
+    plt.yscale('log')
+    plt.subplot(1, 2, 2)
+    plt.title("Val Mean MSE")
+    x = [val_interval * (i + 1) for i in range(len(metric_values))]
+    y = metric_values
+    plt.xlabel("epoch")
+    plt.plot(x, y, color="green")
+    plt.show()
+
+
+# ## Check best pytorch model output with the input image and label
+
+# In[ ]:
+
+
+if is_notebook():
+    model.load_state_dict(torch.load(os.path.join(save_dir, "best_metric_model.pth")))
+    model.eval()
+    with torch.no_grad():
+        # select one image to evaluate and visualize the model output
+        val_input = sample_ds[6]["image"].unsqueeze(0).to(device)
+        mask_indices = [True, False, False, False]
+        val_input[:, mask_indices, ...] = 0
+        val_output = inference(val_input)
+        # val_output = post_trans(val_output[0])
+        plt.figure("image", (24, 6))
+        for i in range(4):
+            plt.subplot(1, 4, i + 1)
+            plt.title(f"image channel {i}")
+            plt.imshow(val_input[0, i, :, :, 70].detach().cpu(), cmap="gray")
+        plt.show()
+        # visualize the 3 channels model output corresponding to this image
+        plt.figure("output mean", (24, 6))
+        for i in range(4):
+            plt.subplot(1, 4, i + 1)
+            plt.title(f"output channel {i}")
+            plt.imshow(val_output[0, i, :, :, 70].detach().cpu(), cmap="gray")
+
+        plt.figure("output std", (24, 6))
+        for i in range(4):
+            plt.subplot(1, 4, i + 1)
+            plt.title(f"output channel {i}")
+            plt.imshow(val_output[0, i+4, :, :, 70].detach().cpu(), cmap="gray")
+        plt.show()
+
+
+# ## Cleanup data directory
+# 
+# Remove directory if a temporary was used.
+
+# In[ ]:
+
+
+if directory is None:
+    shutil.rmtree(root_dir)
+
