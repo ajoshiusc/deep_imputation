b30bb84114724768bfc0edeee6cfba6882716ff9
Sachin Salim <sachinksalim@gmail.com>
2025-02-09 13:20:43 -0800

    added datatransform to scale to 0_1

--- src/utils/transforms.py
+++ src/utils/transforms.py
@@ -51,8 +51,7 @@ class ConvertToMultiChannelBasedOnBratsClassesd(MapTransform):
             d[key] = torch.stack(result, axis=0).float()
         return d
     
-# train crop-size is 64
-contr_syn_transform = {
+contr_syn_transform_default = {
     'train':  Compose([
         # load 4 Nifti images and stack them together
         LoadImaged(keys=["image", "label"]),
@@ -65,7 +64,7 @@ contr_syn_transform = {
             pixdim=(1.0, 1.0, 1.0),
             mode=("bilinear", "nearest"),
         ),
-        RandSpatialCropd(keys=["image", "label"], roi_size=[64, 64, 64], random_size=False),
+        RandSpatialCropd(keys=["image", "label"], roi_size=[224, 224, 144], random_size=False),
         RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=0),
         RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=1),
         RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=2),
@@ -95,71 +94,7 @@ contr_syn_transform = {
     ])
 }
 
-# train crop-size is 224 (larger)
-contr_syn_transform_2 = {
-    'train':  Compose([
-        # load 4 Nifti images and stack them together
-        LoadImaged(keys=["image", "label"]),
-        EnsureChannelFirstd(keys="image"),
-        EnsureTyped(keys=["image", "label"]),
-        ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
-        Orientationd(keys=["image", "label"], axcodes="RAS"),
-        Spacingd(
-            keys=["image", "label"],
-            pixdim=(1.0, 1.0, 1.0),
-            mode=("bilinear", "nearest"),
-        ),
-        RandSpatialCropd(keys=["image", "label"], roi_size=[224, 224, 144], random_size=False),
-        RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=0),
-        RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=1),
-        RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=2),
-        NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
-        RandScaleIntensityd(keys="image", factors=0.1, prob=1.0),
-        RandShiftIntensityd(keys="image", offsets=0.1, prob=1.0),
-    ]),
-    'val': contr_syn_transform['val'],
-    'basic': contr_syn_transform['basic'],
-}
-
-# train crop-size is 224 (larger)
-tumor_seg_transform_default = {
-    'train':  Compose([
-        # load 4 Nifti images and stack them together
-        LoadImaged(keys=["image", "label"]),
-        EnsureChannelFirstd(keys="image"),
-        EnsureTyped(keys=["image", "label"]),
-        ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
-        Orientationd(keys=["image", "label"], axcodes="RAS"),
-        Spacingd(
-            keys=["image", "label"],
-            pixdim=(1.0, 1.0, 1.0),
-            mode=("bilinear", "nearest"),
-        ),
-        RandSpatialCropd(keys=["image", "label"], roi_size=[224, 224, 144], random_size=False),
-        RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=0),
-        RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=1),
-        RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=2),
-        NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
-        RandScaleIntensityd(keys="image", factors=0.1, prob=1.0),
-        RandShiftIntensityd(keys="image", offsets=0.1, prob=1.0),
-    ]),
-    'val': Compose([
-        LoadImaged(keys=["image", "label"]),
-        EnsureChannelFirstd(keys="image"),
-        EnsureTyped(keys=["image", "label"]),
-        ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
-        Orientationd(keys=["image", "label"], axcodes="RAS"),
-        Spacingd(
-            keys=["image", "label"],
-            pixdim=(1.0, 1.0, 1.0),
-            mode=("bilinear", "nearest"),
-        ),
-        NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
-    ]),
-    'basic': contr_syn_transform['basic'],
-}
-
-tumor_seg_transform_scale = {
+contr_syn_transform_scale = {
     'train':  Compose([
         LoadImaged(keys=["image", "label"]),
         EnsureChannelFirstd(keys="image"),
@@ -192,5 +127,9 @@ tumor_seg_transform_scale = {
         ),
         ScaleIntensityd(keys="image", minv=0, maxv=1, channel_wise=True), ## Scale to [0, 1]
     ]),
-    'basic': contr_syn_transform['basic'],
+    'basic': contr_syn_transform_default['basic'],
 }
+
+tumor_seg_transform_default = contr_syn_transform_default
+
+tumor_seg_transform_scale = contr_syn_transform_scale
